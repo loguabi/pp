257c94f74d1f1e275dbaf317f074ac21cf0ac6ef
logitha <loga848.456@gmail.com>
2016-07-02 14:38:57 +0530

    Create pp.c

---
+++ pp.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20]="helloworld",r[20],r1[20];
+int n=2,i,k,j;
+for(i=0;i<=n;i++)
+{
+r[i]=a[i];
+}
+k=strlen(a);
+j=0;
+for(i=((n+n)+l);i<l;)
+{
+r[j]=a[i];
+j++;
+i=i+n;
+}
+printf("%s%s",r,r1);
+getch();
+
+}
